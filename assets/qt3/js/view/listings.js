be1620ed17233cdf13385788eaec6771059733a8
Allan Goff <allangoff@gmail.com>
2026-02-16 13:52:26 -0600

    Refactor listing code into listing.js.

---
+++ assets/qt3/js/view/listings.js
@@ -0,0 +1,119 @@
+// qt3/view/listings.js
+
+import { QT3_LAYOUT } from "../layout.js";
+
+const canvas = document.getElementById("qt3-game");
+const ctx = canvas.getContext("2d");
+
+function setGlobalListStyles() {
+  ctx.setLineDash([0, 0]);
+  ctx.strokeStyle = "#000";
+  ctx.fillStyle = "#000";
+  ctx.font = "16px sans-serif";
+}
+function drawTitle(layout, title) {
+  setGlobalListStyles();
+
+  // Bound box for title.
+  ctx.strokeRect(layout.title.x, layout.title.y, layout.title.w, layout.title.h); 
+  ctx.fillText(title, layout.title.x + 7, layout.title.y + 19);
+  }
+function drawHeader(layout, header) {
+  setGlobalListStyles();
+
+  // Shorthand for layout elements.
+  let row = layout.header;
+  let x_offset = row.h;
+  let o_offset = row.h + row.w;
+
+  // X - Classical.
+  ctx.strokeRect(row.x, row.y, row.h, row.h);
+  ctx.fillText('X', row.x + 9, row.y + 20);
+  ctx.strokeRect(row.x + x_offset, row.y, row.w, row.h);
+  ctx.fillText(header, row.x + x_offset + 8, row.y + 20);
+
+  // O - Classical.
+  ctx.strokeRect(row.x + o_offset, row.y, row.h, row.h);
+  ctx.fillText('O', row.x + o_offset + 8, row.y + 20);
+  ctx.strokeRect(row.x + x_offset + o_offset, row.y, row.w, row.h);
+  ctx.fillText(header, row.x + x_offset + o_offset + 8, row.y + 20);
+  }
+function drawMoveNums(layout) {
+  setGlobalListStyles();
+
+  // Shorthand for layout elements.
+  let row = layout.rows;
+  let sep = row.h;
+
+  for(let i=1; i<=9; i+=2) { // X moves.
+    ctx.strokeRect(row.x, row.y + (i-1)*(sep/2), row.h, row.h);
+    ctx.fillText(i, row.x + 10, row.y + (i-1)*(sep/2) + 19);
+    ctx.strokeRect(row.x + row.h, row.y + (i-1)*(sep/2), row.w, row.h);
+  }
+  let o_offset = row.w + row.h;
+  for(let i=2; i<=8; i+=2) { // O moves.
+    ctx.strokeRect(row.x + row.w+row.h, row.y + (i-2)*(sep/2), row.h, row.h);
+    ctx.fillText(i, row.x + row.w+row.h + 10, row.y + (i-2)*(sep/2) + 19);
+    ctx.strokeRect(row.x + row.h + o_offset, row.y + (i-2)*(sep/2), row.w, row.h);
+  }
+  }
+function drawQuantumMoves(layout, stateString) {
+  setGlobalListStyles();
+  // Need to derive moves from the stateString.
+  let moves = [[4,5], [5,6], [6,4], [7,8], [8,9]];
+
+  let row = layout.rows;
+  let sep = 0;
+  let xory = 1;
+  let offset = 0;
+
+  for(const move of moves) { // X moves.
+    if (xory%2) {
+      offset = 0;
+      ctx.fillText(`${move[0]} --- ${move[1]}`, row.x + row.h + offset + 12, row.y + sep + 19);
+    } 
+    else {                  // O moves.
+      offset = row.w + row.h;
+      ctx.fillText(`${move[0]} --- ${move[1]}`, row.x + row.h + offset + 12, row.y + sep + 19);
+      sep += row.h;
+    }
+    xory += 1;
+  }
+  }
+function drawClassicalMoves(layout, stateString) {
+  setGlobalListStyles();
+  // Need to derive moves from the stateString.
+  let moves = [4, 5, 6, '--', '--', 1, 3, 2];
+
+  let row = layout.rows;
+  let sep = 0;
+  let xory = 1;
+  let offset = 0;
+
+  for(const move of moves) { // X moves.
+    if (xory%2) {
+      offset = 0;
+      ctx.fillText(`    ${move}`, row.x + row.h + offset + 12, row.y + sep + 19);
+    } 
+    else {                  // O moves.
+      offset = row.w + row.h;
+      ctx.fillText(`    ${move}`, row.x + row.h + offset + 12, row.y + sep + 19);
+      sep += row.h;
+    }
+    xory += 1;
+  }
+}
+
+export function drawQuantumListing(layout, stateString) {
+  drawTitle(layout, "       Quantum Moves");
+  drawHeader(layout, "Spooky");
+  drawMoveNums(layout);
+  drawQuantumMoves(layout, stateString);
+  }
+export function drawClassicalListing(layout, stateString) {
+  drawTitle(layout, "       Classical Moves");
+  drawHeader(layout, "  Move");
+  drawMoveNums(layout);
+  drawClassicalMoves(layout, stateString);
+}
+
