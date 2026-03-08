883936f3f70e262b9ccb4eedc91a9d5168a2e2a7
Allan Goff <allangoff@gmail.com>
2026-03-08 17:53:58 -0500

    Delete dead skeleton code from model.js.

--- assets/qt3/js/model/model.js
+++ assets/qt3/js/model/model.js
@@ -1,29 +1,22 @@
 // qt3/model/model.js
 
-import {analyzeStateString,
-} from "./analyzeStateString.js";
-
 /* The QT3 model consists a just a handful of items:
-  A status string (statusString) - the player's next actions.
-  An error string (errorString) - lets players know when they have made a UI mistake.
-  An analysis string (analysisString) - stats about the current game.
-
   The state string (stateString) - human readable history of the game.
-  The history pointer (histPtr) - where we are in the game, undo/redo/restart.
+  A status string (statusString) - the player's next actions.
+  An error string (errorString)  - lets players know when they have made a UI mistake.
 */
 
 let modelStateString  = "";  // This is the state of the game.
 let modelStatusString = "";  // This is the status of the game.
 let modelErrorString  = "";  // Errors if any.
-let modelState;
 
-export function modelSetStateString(str) {  // Sets stateString, model must update its state.
+export function modelSetStateString(str) {  // Sets stateString.
   modelStateString = str;
   }
 
 export function modelGetStateString() {     // Gets stateString.
   return modelStateString;
-  }
+}
 
 export function modelSetStatusString(str) { // Sets statusString.
   modelStatusString = str;
@@ -32,7 +25,7 @@ export function modelSetStatusString(str) { // Sets statusString.
 
 export function modelGetStatusString() {    // Gets statusString.
   return modelStatusString;
-  }
+}
 
 export function modelSetErrorString(str) {  // Sets errorString.
   modelErrorString = str;
@@ -41,44 +34,4 @@ export function modelSetErrorString(str) {  // Sets errorString.
 export function modelGetErrorString() {     // Gets errorString.
   return modelErrorString;
 }
-
-function validStateString(stateString) {
-  // TODO: write validStateString().
-
-  return true;
-}
-
-/* -------------------- */
-
-function processStateChange(str) {
-  modelStateString = str;
-  modelState = analyzeStateString(modelStateString);
-
-  console.log("processStateChange() - modelState", modelState);
-}
-
-function updateStateObjects(stateString) {
-}
-
-function updateStatusString(stateString, stateObjects) {
-  let statusString = "";
-
-  return statusString;
-}
-
-/* -------------------- */
-  // let histPtr = 0;
-
-  // let numOfMoves = 0; // Potential placement/collapses.
-  // const maxNumOfMoves = 9+4; // Potential placement/collapses.
-
-  // export function setHistptr (ptr) {  // 0 => Rerun.
-  //   histPtr = ptr;
-  // }
-
-  // export function slipHistptr (steps) {  // Undo (-1), Redo (+1).
-  //   histPtr += steps;
-  //   if(histPtr < 0) histPtr = 0;
-  //   if(histPtr > numberOfMoves) histPtr = numberOfMoves;
-  // }
 
