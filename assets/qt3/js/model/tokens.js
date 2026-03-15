8483f19d81589137e11618f35b9b8ba1f6aac48c
Allan Goff <allangoff@gmail.com>
2026-03-15 11:03:02 -0500

    Enforce move order in loadGame().

--- assets/qt3/js/model/tokens.js
+++ assets/qt3/js/model/tokens.js
@@ -1,8 +1,17 @@
 // ./assets/qt3/js/model/tokens.js
 
 import { GRAMMAR } from "../model/grammar.js";
+import {ERROR,
+        STATUS,
+} from "../model/statusMsgs.js";
+
+let prevPlayer = "O";
+let prevTurn   = 0;
 
 export function tokenize(stateString) {   // Returns: [{ type: "spooky", token: "" }, ...];
+  prevPlayer = "O";
+  prevTurn   = 0;
+
   const trimmed = stateString.trim();
 
   const tokens = [];
@@ -13,24 +22,38 @@ export function tokenize(stateString) {   // Returns: [{ type: "spooky", token: 
     tokens.push({ type: "empty", token: "" });
     return tokens;
   }
-
+/* Test string for move order.
+X1+(1,2); O2+(2,1)[12]; X2@X1(1)!X1(1)!O2(2); X3+(4,5); O4+(5,4)[34]; X4@X3(4)!X3(4)!O4(5); X5+(7,8); O6+(8,7)[56]; X6@X5(7)!X5(7)!O6(8); {X=1,O=0.5}
+*/
   // --- Parse move fragments.
   if (working !== "") {
     const parts = working.split(";");
+
+    let prevPlayer = "O";
+    let prevTurn   = 0;
 
     for (let raw of parts) {
       let fragment = raw.trim();
       if (!fragment) continue;
 
       let type = null;
+      let match = [];
 
       if(false) { // Order dependence trumps logical sequence.
       } else if (GRAMMAR.spookyToken.test(fragment)) {       type = "spooky";
       } else if (GRAMMAR.degenerateToken.test(fragment)) {   type = "degenerate";
-      } else if (GRAMMAR.placementToken.test(fragment)) {    type = "placement";
-      } else if (GRAMMAR.pureLoopToken.test(fragment)) {     type = "pureLoop";
-      } else if (GRAMMAR.stemLoopToken.test(fragment)) {     type = "stemLoop";
-      } else if (GRAMMAR.loopToken.test(fragment)) {         type = "loop"; // Shadowed by previous pair of blocks.
+      } else if (GRAMMAR.pureLoopToken.test(fragment)) {
+        if(isSequential(fragment))                           type = "pureLoop";
+        else                                                 type = "invalid";
+      } else if (GRAMMAR.stemLoopToken.test(fragment)) {
+        if(isSequential(fragment))                           type = "stemLoop";
+        else                                                 type = "invalid";
+      } else if (GRAMMAR.loopToken.test(fragment)) {
+        if(isSequential(fragment))                           type = "loop"; // Shadowed by previous pair of blocks.
+        else                                                 type = "invalid";
+      } else if (GRAMMAR.placementToken.test(fragment)) {
+        if(isSequential(fragment))                           type = "placement";
+        else                                                 type = "invalid";
       } else if (GRAMMAR.selfCollapseToken.test(fragment)) { type = "selfCollapse";
       } else if (GRAMMAR.collapseToken.test(fragment)) {     type = "collapse";
       } else if (GRAMMAR.scoreToken.test(fragment)) {        type = "score";  // Path taken only on multiple tokens.
@@ -38,10 +61,50 @@ export function tokenize(stateString) {   // Returns: [{ type: "spooky", token: 
       }
 
       tokens.push({ type, token: fragment });
+      if(type === "invalid") {
+        break;
+      } 
     }
   }
 
   return tokens;   // Returns: [{ type: "spooky", token: "" }];
+}
+
+function isSequential(fragment) {
+  let sequential = true;
+  let match = [];
+  let player = "";
+  let turn   = 0;
+
+  match = GRAMMAR.placementToken.exec(fragment);
+  if(match != null) {
+    player = match[1];
+    turn   = Number(match[2]);
+  }
+  match = GRAMMAR.pureLoopToken.exec(fragment);
+  if(match != null) {
+    player = match[1];
+    turn   = Number(match[2]);
+  }
+  match = GRAMMAR.stemLoopToken.exec(fragment);
+  if(match != null) {
+    player = match[1];
+    turn   = Number(match[2]);
+  }
+  // match = GRAMMAR.loopToken.exec(fragment);
+  // if(match != null) {
+  //   player = match[1];
+  //   turn   = Number(match[2]);
+  // }
+
+  if((player === prevPlayer)
+  || (turn != prevTurn + 1)) {
+    sequential = false;
+  }
+  prevPlayer = player;
+  prevTurn   = turn;
+
+  return sequential;
 }
 
 export function tokensToString(tokens, tokenLimit = null) {
--- assets/qt3/js/tests/tokens.test.js
+++ assets/qt3/js/tests/tokens.test.js
@@ -19,8 +19,8 @@ let fragmentTests = [
   { str: ".", type: "invalid"},
   { str: "X1+(3", type: "spooky"},
   { str: "X1+(3,8)", type: "placement"},
-  { str: "O2+(1,2)[12]", type: "pureLoop"},
-  { str: "X3+(2,3)[23|1]", type: "stemLoop"},
+  { str: "X1+(1,2); O2+(1,2)[12]", type: "pureLoop"},
+  { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]", type: "stemLoop"},
   { str: "X2@O2(1)!X1(2)!O2(1)", type: "collapse"},
   { str: "X9+(5,5)", type: "degenerate"},
   { str: "O9@X9(5)!X9(5)", type: "selfCollapse"},
@@ -30,8 +30,9 @@ N = fragmentTests.length;
 
 for (let test of fragmentTests) {
   const parse = tokenize(test.str);   // Returns: [{ type: "spooky", token: "" }];
+  let last = parse[parse.length - 1];
 
-  assertEqual(parse[0].type, test.type);
+  assertEqual(last.type, test.type);
 }
 
 console.log(`tokenize(fragments)   ${N}/${N} tests passed`);
@@ -40,36 +41,51 @@ console.log(`tokenize(fragments)   ${N}/${N} tests passed`);
 
 let stringTests = [ 
   // Valid strings.
-  { str: "", type: ["empty"]},
-  { str: "{X=2, O=0}", type: ["score"]},
-  { str: "X9+(5,5); {X=2, O=0}", type: ["degenerate", "score"]},
-  { str: "X1+(1,2); O2+(1;", type: ["placement", "spooky"]},
-  { str: "X1+(1,2); O2+(1,2)[12]; X2@O2(2)!X1(1)!O2(2); ", type: ["placement", "pureLoop", "collapse"]},
-  { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); ", 
-    type: ["placement", "placement", "stemLoop", "collapse"]},
-  { str: "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
-    type: ["placement", "pureLoop", "collapse", "degenerate", "selfCollapse", "score"]},
+    { str: "", type: ["empty"]},
+    { str: "{X=2, O=0}", type: ["score"]},
+    { str: "X9+(5,5); {X=2, O=0}", type: ["degenerate", "score"]},
+    { str: "X1+(1,2); O2+(1;", type: ["placement", "spooky"]},
+    { str: "X1+(1,2); O2+(1,2)[12]; X2@O2(2)!X1(1)!O2(2); ", type: ["placement", "pureLoop", "collapse"]},
+    { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); ", 
+      type: ["placement", "placement", "stemLoop", "collapse"]},
+    { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+        + "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
+      type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+            "placement", "pureLoop", "collapse", "degenerate", "selfCollapse", "score"]},
 
   // Invalid strings.
-  { str: "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=}", 
-    type: ["placement", "pureLoop", "collapse", "degenerate", "selfCollapse", "invalid"]},
-  { str: "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X95); {X=2, O=0}", 
-    type: ["placement", "pureLoop", "collapse", "degenerate", "invalid", "score"]},
-  { str: "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5); O9@X9(5)!X9(5); {X=2, O=0}", 
-    type: ["placement", "pureLoop", "collapse", "invalid", "selfCollapse", "score"]},
-  { str: "X7+(4,7); O8+(4,7)[78]; X87)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
-    type: ["placement", "pureLoop", "invalid", "degenerate", "selfCollapse", "score"]},
-  { str: "X7+(4,7); O8+(4,7)[7; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
-    type: ["placement", "invalid", "collapse", "degenerate", "selfCollapse", "score"]},
-  { str: "X7(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
-    type: ["invalid", "pureLoop", "collapse", "degenerate", "selfCollapse", "score"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+       + "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "placement", "pureLoop", "collapse", "degenerate", "selfCollapse", "invalid"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+       + "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X95); {X=2, O=0}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "placement", "pureLoop", "collapse", "degenerate", "invalid", "score"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+       + "X7+(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5); O9@X9(5)!X9(5); {X=2, O=0}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "placement", "pureLoop", "collapse", "invalid", "selfCollapse", "score"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+       + "X7+(4,7); O8+(4,7)[78]; X87)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "placement", "pureLoop", "invalid", "degenerate", "selfCollapse", "score"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+      + "X7+(4,7); O8+(4,7)[7; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "placement", "invalid", "collapse", "degenerate", "selfCollapse", "score"]},
+  { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,1)[165432]; X6@X1(1)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); "
+      + "X7(4,7); O8+(4,7)[78]; X8@X7(7)!X7(7)!O8(4); X9+(5,5); O9@X9(5)!X9(5); {X=2, O=0}", 
+    type: ["placement", "placement", "placement", "placement", "placement", "pureLoop", "collapse",
+           "invalid", "pureLoop", "collapse", "degenerate", "selfCollapse", "score"]},
 
   // Corruption pairs:
-  { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); O4+(4,5); X5+(5,6); O6+(9", 
-    type: ["placement", "placement", "stemLoop", "collapse", "placement", "placement", "spooky"]},
-  { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); O4+(); X5+(5,6); O6+(9", 
-    type: ["placement", "placement", "stemLoop", "collapse", "invalid", "placement", "spooky"]},
-
+    { str: "X1+(1,2); O2+(2,3); X3+(3,6); O4+(6,9); X5+(9,8); O6+(8,9)[56|1234]; X6@X5(9)!X1(1)!O2(2)!X3(3)!O4(6)!X5(9)!O6(8); X7+(7,4); O8+(4,5); X9+(5", 
+      type: ["placement", "placement", "placement", "placement", "placement", "stemLoop", "collapse", "placement", "placement", "spooky"]},
+    { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); O4+(4,5); X5+(5,6); O6+(9", 
+      type: ["placement", "placement", "stemLoop", "collapse", "placement", "placement", "spooky"]},
+    { str: "X1+(1,2); O2+(2,3); X3+(2,3)[23|1]; O3@O2(2)!X1(1)!O2(2)!X3(3); O4+(); X5+(5,6); O6+(9", 
+      type: ["placement", "placement", "stemLoop", "collapse", "invalid", "placement", "spooky"]},
   ];
 N = stringTests.length;
 
