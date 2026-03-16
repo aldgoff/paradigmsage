3df0d6116a0b5e43f5b17bda9882346cdfd4c3e9
Allan Goff <allangoff@gmail.com>
2026-03-16 15:17:31 -0500

    Basic 3dc site infrastructure.

---
+++ assets/3dc/js/main.js
@@ -0,0 +1,13 @@
+// ./assets/3dc/js/main.js
+
+// import { initController } from "./controller/controller.js";
+
+console.log("main.js: 3dc/js/main.js");
+
+// initController();
+
+if (import.meta.env?.DEV ?? true) { // Regression tests of the model.
+  // import("./tests/addSpookyMove.test.js");
+}
+
+console.log("--------------------");
---
+++ content/3dc/_index.md
@@ -0,0 +1,6 @@
+---
+title: "3D Chess"
+---
+
+This version of 3D chess uses planar moves and advancement squares.
+It claims that w/o these two paradigm busting concepts, play in 3D cannot feel like chess.
