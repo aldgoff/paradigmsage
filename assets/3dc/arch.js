eb63570916a7b552d5970538b681ad18290fc7b9
Allan Goff <allangoff@gmail.com>
2026-04-13 11:39:58 -0500

    Create obsolete folder for 3dc.

--- assets/3dc/arch.js
+++ assets/3dc/arch.js
@@ -4,55 +4,32 @@
   Author: Allan Goff
   Date: 4/03/26
   UI: none, just documentation in code form
-  */
+*/
 
-/* Tree - 4/9/26.
-MacBookPro:paradigmsage adgoff$ tree assets/3dc/view
-assets/3dc/view
-├── _tbd.md
-├── board
-├── brainstorming.md
-├── decorators
-│   ├── decorators.js
-│   ├── decorators.json
-│   └── decorators.md
-├── demos.js
-├── pieces
-├── registerHandlers.js
-├── render
-│   ├── cameras.js
-│   ├── cameras.json
-│   ├── cameras.md
-│   ├── coordMaps.md
-│   ├── coordsMaps.js
-│   ├── coordsMaps.json
-│   ├── lights.js
-│   ├── lights.json
-│   ├── lights.md
-│   ├── obsolete
-│   │   └── initThree.js
-│   ├── renders.js
-│   ├── renders.json
-│   ├── renders.md
-│   ├── scenes.js
-│   ├── scenes.json
-│   └── scenes.md
-├── tiles
-│   ├── tiles.js
-│   ├── tiles.json
-│   └── tiles.md
-├── trays
-├── view.js
-└── view.md
-
-7 directories, 28 files
- */
+/* Tree - 4/91326.
+  MacBookPro:paradigmsage adgoff$ tree -L 1 assets/3dc
+  assets/3dc
+  ├── arch.js
+  ├── controller
+  ├── dev
+  ├── exampleRegistration
+  ├── foundation
+  ├── geometry
+  ├── layout.js
+  ├── layout.md
+  ├── main.js
+  ├── model
+  ├── templates
+  ├── tests
+  └── view
+  9 directories, 4 files
+*/
 
 /* Control Flow:
   play.md - web page
     - imports the 3D render engine (Three), 
-    - places each 2D canvas on the page (top, left).
-    - defines each canvas
+    - places each 2D MOD panel on the page (top, left).
+    - defines each panel
       - DOM id (*-window)
       - canvas id (3dc-*), (width, height)
   main.js - Entry point.
@@ -60,6 +37,5 @@ assets/3dc/view
       - model.init().
       - view.init().
     - Regressions tests (tests/core/asserts.js) via imports (viewable in local console).
-
- */
+*/
 
